0fed6c8a4f2cc0e56d626287e8c3d55a0e5911e3
Timo <pottimo@gmail.com>
2024-06-26 09:14:08 +0200

    even better structure

--- src/apps/lmz/main.ts
+++ src/apps/lmz/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './lmz/app.config';
-import { AppComponent } from './lmz/app.component';
+import { appConfig } from './app.config';
+import { AppComponent } from './app.component';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
