b620a3633e442f6878dcf18654f8f477e393615d
Timo Pot <pottimo@gmail.com>
2024-06-26 13:16:49 +0200

    Feature/normalise (#4)
    * new structure with ui lib
    
    * add normalise

--- projects/lmz/app.component.ts
+++ projects/lmz/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Hallo Martin, dit is `Laat Maar Zien Dan`';
+  title = 'Hallo Timo, dit is `Laat Maar Zien Dan`';
 }
