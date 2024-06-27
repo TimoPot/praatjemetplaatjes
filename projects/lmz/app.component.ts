6a6a426e5d68c9b26c664722fcff174b2ed96195
Timo Pot <pottimo@gmail.com>
2024-06-27 20:55:42 +0200

    Feature/UI container (#7)
    * new structure with ui lib
    
    * add normalise
    
    * cleaning and minimal start
    
    * fix
    
    * add a start for components and routing

--- projects/lmz/app.component.ts
+++ projects/lmz/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UiComponent } from '@ui/components/ui.component';
+import { MainComponent } from '@ui/components/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [RouterOutlet, UiComponent, MainComponent],
 })
 export class AppComponent {
   title = 'Laat Maar Zien`';
---
+++ projects/ui/src/components/main/main.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-main',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MainComponent { }
