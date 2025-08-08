bc94ef15ab9dab171b59765cb250fbacae08b703
Timo <pottimo@gmail.com>
2025-08-08 10:42:46 +0200

    update to angular 20

--- projects/lmz/components/main/main.component.ts
+++ projects/lmz/components/main/main.component.ts
@@ -1,6 +1,6 @@
 import { UiMainComponent } from '@ui/components/main/main.component';
 import { UiTopbarComponent } from '@ui/components/topbar/topbar.component';
-import { CommonModule } from '@angular/common';
+
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
@@ -8,6 +8,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     templateUrl: './main.component.html',
     styleUrl: './main.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [CommonModule, UiMainComponent, UiTopbarComponent]
+    imports: [UiMainComponent, UiTopbarComponent]
 })
 export class MainComponent {}
