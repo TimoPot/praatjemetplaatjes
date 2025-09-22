bfae31d33d205f71a68f4ae18150be579d9d0ed5
Timo Pot <pottimo@gmail.com>
2025-09-22 19:13:53 +0200

    remove old imports

--- projects/lmz/app.component.ts
+++ projects/lmz/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UiComponent } from '@ui/components/ui.component';
-import { UiMainComponent } from '@ui/components/main/main.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, UiComponent, UiMainComponent]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   title = 'Laat Maar Zien`';
