c3dce9b3f7a0faa4fbe2b919b156700647c9b87a
Timo Pot <pottimo@gmail.com>
2025-10-08 19:10:11 +0200

    wip

--- projects/lmz/pages/categories/categories.ts
+++ projects/lmz/pages/categories/categories.ts
@@ -9,13 +9,51 @@ import { Cards } from 'projects/lmz/components/cards/cards';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Categories {
-  data = signal([
+  categories = signal([
     {
+      id: 1,
+      isHoofdCategory: true,
       category: 'alledaagse dingen',
       text: 'Alle daagse dingen',
       src: './images/huis.jpg',
+      subCategorieen: [2],
     },
-    { category: 'huis', text: 'Huis', src: './images/boom.jpg' },
-    { category: 'keuken', text: 'Keuken', src: './images/auto.png' },
+    {
+      id: 2,
+      isHoofdCategory: false,
+      category: 'huis',
+      text: 'Huis',
+      src: './images/boom.jpg',
+      subCategorieen: [3, 4],
+    },
+    {
+      id: 3,
+      isHoofdCategory: false,
+      category: 'keuken',
+      text: 'Keuken',
+      src: './images/keuken.png',
+    },
+    {
+      id: 4,
+      isHoofdCategory: false,
+      category: 'woonkamer',
+      text: 'Woonkamer',
+      src: './images/woonkamer.png',
+    },
+    {
+      id: 5,
+      isHoofdCategory: true,
+      category: 'natuur',
+      text: 'Natuur',
+      src: './images/natuur.png',
+      subCategorieen: [6],
+    },
+    {
+      id: 6,
+      isHoofdCategory: false,
+      category: 'water',
+      text: 'Water',
+      src: './images/water.png',
+    },
   ]);
 }
