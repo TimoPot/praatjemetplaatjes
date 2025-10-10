5524accecb499442e789561026d0c65e1d1ea37a
Timo Pot <pottimo@gmail.com>
2025-10-10 20:43:00 +0200

    corrected title on main

--- projects/pmp/pages/categories/categories.ts
+++ projects/pmp/pages/categories/categories.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Cards } from 'projects/pmp/components/cards/cards';
 import { CategoriesService } from 'projects/pmp/shared/data-access/categories-service';
 
@@ -10,8 +11,25 @@ import { CategoriesService } from 'projects/pmp/shared/data-access/categories-se
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Categories {
+  private route = inject(ActivatedRoute);
   private categoriesService = inject(CategoriesService);
 
   // Only show hoofd categories (= main categories)
   mainCategories = this.categoriesService.mainCategories;
+
+  constructor() {
+    // React to route parameter changes
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      this.setSelectedCategory(id ? Number(id) : undefined);
+    });
+  }
+
+  /**
+   * set the selected category id in the state
+   */
+  private setSelectedCategory(id: number | undefined) {
+    console.log('Setting selected category to:', id);
+    this.categoriesService.select$.next(id ? Number(id) : undefined);
+  }
 }
