e1b2f9b9bdaf1b0e3b89cf5308931d59bee56dd8
Timo Pot <pottimo@gmail.com>
2025-10-20 14:45:03 +0200

    better links ui

--- projects/pmp/app.component.ts
+++ projects/pmp/app.component.ts
@@ -9,4 +9,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Laat Maar Zien`';
+
+  // CON: laptops with touch screens are considere mobile also
+  // isMobile = this._isMobile();
+
+  // private _isMobile(): boolean {
+  //   // Check for maxTouchPoints > 0 (modern browsers)
+  //   const check = 'maxTouchPoints' in navigator && navigator.maxTouchPoints > 0;
+  //   console.log(`isMobile: ${check}`);
+  //   return check;
+  // }
 }
