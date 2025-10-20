870d608ea9c15096869be572aa60ac6ab946a7cf
Timo Pot <pottimo@gmail.com>
2025-10-20 18:21:43 +0200

    add canceling active playing word

--- projects/pmp/shared/services/speech-service.ts
+++ projects/pmp/shared/services/speech-service.ts
@@ -8,6 +8,11 @@ export class SpeechService {
   speak(text: string) {
     if (!text) return;
 
+    //  **Cancel** any speech currently in progress
+    if (this.synth.speaking) {
+      this.synth.cancel();
+    }
+
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.lang = this.language;
     utterance.rate = 0.7; // Speed of speech
