d76b262190781ff081aa063b7aa986504a91a6c9
Timo Pot <pottimo@gmail.com>
2025-11-06 20:20:11 +0100

    improved speech service

--- projects/pmp/shared/services/speech-service.ts
+++ projects/pmp/shared/services/speech-service.ts
@@ -4,23 +4,31 @@ import { Injectable } from '@angular/core';
 export class SpeechService {
   private synth = window.speechSynthesis;
   private language = 'nl-NL';
+  private voice: SpeechSynthesisVoice | null = null;
+
+  constructor() {
+    this.synth.onvoiceschanged = () => {
+      const voices = this.synth.getVoices();
+      const dutchVoice = voices.find((v) => v.lang === this.language);
+      if (dutchVoice) {
+        this.voice = dutchVoice;
+      }
+    };
+  }
 
   speak(text: string) {
     if (!text) return;
 
-    //  **Cancel** any speech currently in progress
     if (this.synth.speaking) {
+      //  **Cancel** any speech currently in progress
       this.synth.cancel();
     }
-
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.lang = this.language;
-    utterance.rate = 0.7; // Speed of speech
+    utterance.rate = 0.65; // Speed of speech
     utterance.pitch = 1; // Pitch of the voice
     utterance.volume = 1; // Volume (0 to 1)
-    utterance.voice =
-      this.synth.getVoices().find((voice) => voice.lang === this.language) ||
-      null;
+    utterance.voice = this.voice || null;
     this.synth.speak(utterance);
   }
 }
