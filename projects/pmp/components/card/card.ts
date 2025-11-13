4a6d3efa453ae16c22cc79bd29df7b154120e135
Timo Pot <pottimo@gmail.com>
2025-11-13 19:21:02 +0100

    Refactor card and navigation components to capitalize names and remove uppercase styling

--- projects/pmp/components/card/card.ts
+++ projects/pmp/components/card/card.ts
@@ -25,8 +25,8 @@ export class Card {
 
   boardService = inject(BoardService);
 
-  capitalizedName = computed(() => {
-    const name = this.card()!.name;
-    return name.charAt(0).toUpperCase() + name.slice(1);
-  });
+  // capitalizedName = computed(() => {
+  //   const name = this.card()!.name;
+  //   return name.charAt(0).toUpperCase() + name.slice(1);
+  // });
 }
